dda465715689975ae1b8b85ca820f4cb8dee9b0b
Caitlin Cradick <caitlincradick@gmail.com>
2023-09-12 16:41:58 -0700

    add the initial api call

---
+++ src/apiCalls.js
@@ -0,0 +1,5 @@
+export function getArticles() {
+  fetch('https://newsapi.org/v2/everything?q=cats OR vanderpump OR climate&from=2023-09-11&to=2023-09-11&language=en&apiKey=6af7ab5557284ac88194bcaa9f8ec36b')
+  .then(res => res.json())
+
+}
