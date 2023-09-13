ca141eec199c1c6cd06e8e890a00be36c66cc561
Caitlin Cradick <caitlincradick@gmail.com>
2023-09-12 21:10:06 -0700

    add api calls and implement into functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,15 @@ import { useState, useEffect } from 'react';
 function App() {
   const [isClicked, setIsClicked] = useState(false)
   const [title, setTitle] = useState("")
-
+  const [articles, setArticles] = useState([])
+  
+  console.log("article", articles)
+  
+  useEffect(() => {
+   getArticles()
+   .then(d => setArticles(d.articles))
+  }, [])
+ 
 
 
 
@@ -20,8 +28,8 @@ console.log(isClicked)
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Homepage data={data.articles} isClicked={isClicked} setIsClicked={setIsClicked} title={title} setTitle={setTitle}/>} />
-        <Route path='/article' element={<DetailedPage data={data.articles} isClicked={isClicked} setIsClicked={setIsClicked} title={title} setTitle={setTitle}/>} />
+        <Route path='/' element={<Homepage data={articles} isClicked={isClicked} setIsClicked={setIsClicked} title={title} setTitle={setTitle}/>} />
+        <Route path='/article' element={<DetailedPage data={articles} isClicked={isClicked} setIsClicked={setIsClicked} title={title} setTitle={setTitle}/>} />
       </Routes>
     </div>
   );
