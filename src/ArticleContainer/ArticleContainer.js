6ef738bea7c9505d2eb3f50c8ff9bb4b85a6d53a
Caitlin Cradick <caitlincradick@gmail.com>
2023-09-13 17:38:14 -0700

    reorganize article structure

--- src/ArticleContainer/ArticleContainer.js
+++ src/ArticleContainer/ArticleContainer.js
@@ -11,31 +11,39 @@ const ArticleContainer = ({data, filter, isClicked, setIsClicked, title, setTitl
       setIsClicked(true)
       setTitle(title)
       setSource(source.name)
+      // setSingleArticle(article)
   }
 
+  // if (
+  //   article.title &&
+  //   article.description &&
+  //   (article.title.toLowerCase().includes(filter) || article.description.toLowerCase().includes(filter))
+  // ) {
   //make set the state for title then say if the title matches the title in the data display just that title or whatever 
 
   const singleArticle = data.map(article => {
     if(article.title.toLowerCase().includes(filter) || article.description.toLowerCase().includes(filter)) {
       const key = uuidv4()
-  return (
-    <NavLink to="/article" key={key} style={{textDecoration:'none', color:'black'}}>
-    <div className='home-single' key={key} value={article.title} onClick={() => clicked(article.title, article.source)} >
-      <h3 className='home-title'>{article.title}</h3>
-      <img className='home-image'src={article.urlToImage} alt={article.title}></img>
-      <p className='home-date'>{article.publishedAt.slice(0,10)}</p>
-      <p className='home-description'>{article.description}</p>
-    </div>
-     </NavLink>
-  )
-}
-})
+      return (
+        <NavLink to="/article" key={key} style={{ textDecoration: 'none', color: 'black' }}>
+          <div className='home-single' key={key} value={article.title} onClick={() => clicked(article.title, article.source)} >
+            <p className='home-date'>{article.publishedAt.slice(0, 10)}</p>
+            <img className='home-image' src={article.urlToImage} alt={article.title}></img>
+            <h3 className='home-title'>{article.title}</h3>
+            <p className='home-description'>{article.description}</p>
+          </div>
+        </NavLink>
+      )
+    } else if (!article){
+      <p>nada nada </p>
+    }
+  })
 
-return (
-  <div className='article-container'>
-    {singleArticle}
-  </div>
-)
+  return (
+    <div className='article-container'>
+      {singleArticle}
+    </div>
+  )
 }
 
 export default ArticleContainer
