b988d2a210bfe5d0a2cc47cacaf482c69ae5f925
Caitlin Cradick <caitlincradick@gmail.com>
2023-09-13 17:39:57 -0700

    update API call to include apple and telsa

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
 export function getArticles(today, threeDaysAgo) {
-  return fetch(`https://newsapi.org/v2/everything?q=cats OR vanderpump OR climate&from=${threeDaysAgo}&to=${today}&sortBy=publishedAt&language=en&apiKey=6af7ab5557284ac88194bcaa9f8ec36b`)
+  return fetch(`https://newsapi.org/v2/everything?q=tesla OR apple OR climate&from=${threeDaysAgo}&to=${today}&sortBy=publishedAt&language=en&apiKey=6af7ab5557284ac88194bcaa9f8ec36b`)
   .then(res => res.json())
 }
